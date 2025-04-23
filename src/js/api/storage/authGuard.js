fa2987559d93a74f10cc7fe45d5b69cffa4826ef
fantadiallo <fatmbo0003@outlook.com>
2025-04-23 22:42:19 +0200

    chore(storage): add API headers, constants, and storage helpers

---
+++ src/js/api/storage/authGuard.js
@@ -0,0 +1,24 @@
+import { ACCESS_TOKEN_KEY } from "./constans";
+import * as storage from "./key";
+save
+/**
+ * Checks if the user is authenticated and the session is still valid.
+ * If valid, executes a callback; otherwise, redirects to login.
+ *
+ * @param {Function} callback - Function to run if the user is authenticated and session is valid.
+ */
+export function authGuard(callback) {
+    const token = storage.load(ACCESS_TOKEN_KEY);
+    const expiresAt = storage.load("sessionExpiresAt");
+  
+    const now = Date.now();
+  
+    if (!token || !expiresAt || now > expiresAt) {
+      storage.remove(ACCESS_TOKEN_KEY);
+      storage.remove("user");
+      storage.remove("sessionExpiresAt");
+      window.location.href = ROUTES.LOGIN;
+    } else if (typeof callback === "function") {
+      callback();
+    }
+  }
